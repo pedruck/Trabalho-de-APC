8a7f5ee1f23f77093a08713a32621ec2c3951242
pedruck <pedrodmreis@gmail.com>
2024-06-15 17:48:08 -0300

    agr trabalhem amigos

--- TrabalhoAPC/main.c
+++ TrabalhoAPC/main.c
@@ -1,5 +1,6 @@
 
 #include "include/raylib.h"
+#include "include/lbasic.h"
 
 //#define PLATFORM_WEB
 
@@ -8,12 +9,48 @@
 #endif
 
 
-const int screenWidth = 1280;
+const int screenWidth = 1680;
 const int screenHeight = 720;
+
+
+
+Vetor2D LocalizacaoMouse;
 
 Texture2D bg1;
 Texture2D division;
 Texture2D bg1bar;
+Texture2D passaporteText;
+
+Vector2 NomeLocation;
+
+Retangulo passaporte;
+
+Font font;
+
+bool AvalicaoFoiFeita;
+
+
+
+void UpdateFrames(void)
+{
+
+     if(AvalicaoFoiFeita == false)
+    {
+
+        passaporteText = LoadTexture("Textures/passport-blank.png");
+        
+    }
+
+}
+
+
+
+
+
+
+
+
+
 
 //----------------------------------------------------------------------------------
 // Module functions declaration
@@ -25,6 +62,7 @@ void UpdateDrawFrame(void);     // Update and Draw one frame
 //------------------------------------------------------------------------------------
 int WinMain(void)
 {
+    
     // Initialization
     //--------------------------------------------------------------------------------------
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
@@ -32,6 +70,19 @@ int WinMain(void)
     bg1 = LoadTexture("Textures/p-p-bg1_scaled.png");
     division = LoadTexture("Textures/p-p-division_scaled.png");
     bg1bar = LoadTexture("Textures/p-p-bar1_scaled.png");
+    font = LoadFont("Textures/BMmini.TTF");
+    UpdateFrames();
+
+    AvalicaoFoiFeita = false;
+
+    passaporte.x = 900;
+    passaporte.y = 350;
+    passaporte.largura = passaporteText.width;
+    passaporte.altura = passaporteText.height;
+
+    
+
+    
 
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
@@ -43,6 +94,7 @@ int WinMain(void)
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         UpdateDrawFrame();
+
     }
 #endif
 
@@ -59,9 +111,32 @@ int WinMain(void)
 //----------------------------------------------------------------------------------
 void UpdateDrawFrame(void)
 {
+    
     // Update
     //----------------------------------------------------------------------------------
-    // TODO: Update your variables here
+
+    LocalizacaoMouse.x = GetMouseX();
+    LocalizacaoMouse.y = GetMouseY();
+
+    NomeLocation.x = passaporte.x + 30;
+    NomeLocation.y = passaporte.y + 340;
+
+    
+
+    // comeco da logica do passaporte
+
+    if (ChecagemColisaoPontoRetangulo(LocalizacaoMouse, passaporte) == true && MouseDown(0) == true && LocalizacaoMouse.x > 850 && (LocalizacaoMouse.y < screenHeight && LocalizacaoMouse.y > 0) && LocalizacaoMouse.x < screenWidth)
+    {
+        
+        passaporte.x = LocalizacaoMouse.x - passaporte.largura/2;
+        passaporte.y = LocalizacaoMouse.y - passaporte.altura/2;
+
+    }
+
+    // fim da logica do passaporte
+
+
+    
     //----------------------------------------------------------------------------------
 
     // Draw
@@ -71,12 +146,17 @@ void UpdateDrawFrame(void)
         ClearBackground(BLACK);
 
         
-
+        
         
         DrawTexture(bg1, 0, 0, WHITE);
         DrawTexture(bg1bar, 0, 0, WHITE);
         DrawTexture(division, 0, 0, WHITE);
+        DrawTexture(passaporteText, passaporte.x, passaporte.y, WHITE);
+        DrawText("Nome do cara", passaporte.x + 30, passaporte.y + 340 , 20, BLACK);
 
+
+        
+        
         DrawText("DATA:", 0, 0, 20, LIGHTGRAY);
       //  DrawText(" DATA:", 0, 0, 20, LIGHTGRAY);
         
@@ -84,3 +164,5 @@ void UpdateDrawFrame(void)
     EndDrawing();
     //----------------------------------------------------------------------------------
 }
+
+
