61fb26ac33af51c3d284afcb736b658c4f42e18a
pedruck <pedrodmreis@gmail.com>
2024-06-21 17:24:04 -0300

    continuei o sistema das pessoas la

--- TrabalhoAPC/main.c
+++ TrabalhoAPC/main.c
@@ -1,8 +1,9 @@
 
 #include "include/raylib.h"
 #include "include/lbasic.h"
+#include "include/pessoas.h"
 
-//#define PLATFORM_WEB
+
 
 #if defined(PLATFORM_WEB)
     #include <emscripten/emscripten.h>
@@ -11,6 +12,8 @@
 
 const int screenWidth = 1680;
 const int screenHeight = 720;
+
+int Count;
 
 
 
@@ -22,11 +25,15 @@ Texture2D bg1bar;
 Texture2D passaporteText;
 Texture2D CrosshairText;
 
+
     
 
 Vector2 NomeLocation;
 
 Vetor2D Crosshair;
+
+Vector2 PessoaPosition;
+
 Retangulo passaporte;
 
 Font font;
@@ -38,12 +45,14 @@ bool AvalicaoFoiFeita;
 void UpdateFrames(void)
 {
 
-     if(AvalicaoFoiFeita == false)
+    if(AvalicaoFoiFeita == false)
     {
 
         passaporteText = LoadTexture("Textures/passport-blank.png");
         
     }
+
+    
 
 }
 
@@ -51,20 +60,18 @@ void UpdateFrames(void)
 
 
 
-//----------------------------------------------------------------------------------
-// Module functions declaration
-//----------------------------------------------------------------------------------
-void UpdateDrawFrame(void);     // Update and Draw one frame
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
+void UpdateDrawFrame(void);     // Update 
+
+
 int WinMain(void)
 {
+    Count = 0;
     
-    // Initialization
+    // Inicializacao
     //--------------------------------------------------------------------------------------
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    
 
     bg1 = LoadTexture("Textures/p-p-bg1_scaled.png");
     division = LoadTexture("Textures/p-p-division_scaled.png");
@@ -72,6 +79,8 @@ int WinMain(void)
     font = LoadFont("Textures/BMmini.TTF");
     CrosshairText = LoadTexture("Textures/Cursor.png");
     
+    
+
     DisableCursor();
    
     UpdateFrames();
@@ -80,8 +89,14 @@ int WinMain(void)
 
     passaporte.x = 900;
     passaporte.y = 350;
+
+    PessoaPosition.x = -50;
+    PessoaPosition.y = 82;
+
     passaporte.largura = passaporteText.width;
     passaporte.altura = passaporteText.height;
+
+    LoadPessoas(Count);
     
 
     
@@ -142,6 +157,7 @@ void UpdateDrawFrame(void)
     {
         
         passaporte.x = LocalizacaoMouse.x - passaporte.largura/2;
+
         passaporte.y = LocalizacaoMouse.y - passaporte.altura/2;
 
     }
@@ -149,7 +165,19 @@ void UpdateDrawFrame(void)
     // fim da logica do passaporte
 
 
-    
+    // ------------------------- CODIGO TEMPORARIO (DEBUG) -----------------------------
+
+    if(IsMouseButtonPressed(0))
+    {
+
+        Count++;
+        LoadPessoas(Count);
+
+    }
+
+
+    //----------------------------------------------------------------------------------
+
     //----------------------------------------------------------------------------------
 
     // Draw
@@ -157,23 +185,18 @@ void UpdateDrawFrame(void)
     BeginDrawing();
 
         ClearBackground(BLACK);
-
-
-        
-        
+  
         DrawTexture(bg1, 0, 0, WHITE);
+        DrawTextureEx(PessoaSprite, PessoaPosition, 0.0, 1.18, WHITE);
         DrawTexture(bg1bar, 0, 0, WHITE);
         DrawTexture(division, 0, 0, WHITE);
         DrawTexture(passaporteText, passaporte.x, passaporte.y, WHITE);
         DrawTexture(CrosshairText, LocalizacaoMouse.x - CrosshairText.width/2, LocalizacaoMouse.y - CrosshairText.height/2, WHITE);
-        DrawText("Nome do cara", passaporte.x + 30, passaporte.y + 340 , 20, BLACK);
-
-
         
+        DrawText("Nome do cara", passaporte.x + 30, passaporte.y + 340 , 20, BLACK);
         
         DrawText("DATA:", 0, 0, 20, LIGHTGRAY);
       //  DrawText(" DATA:", 0, 0, 20, LIGHTGRAY);
-        
 
     EndDrawing();
     //----------------------------------------------------------------------------------
